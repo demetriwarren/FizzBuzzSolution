aeb146cc51c805e5b8ce43f191a556d6d4b6a4da
Demetri Warren <dwarren@tql.com>
2024-06-13 14:12:30 -0400

    Add project files.

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace FizzBuzz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(var i = 1; i <= 30; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine($"{i}: FIZZBUZZ");
+                } else if (i % 3 == 0)
+                {
+                    Console.WriteLine($"{i}: FIZZ ");
+                } else if (i % 5 == 0)
+                {
+                    Console.WriteLine($"{i}: BUZZ ");
+                } else
+                {
+                    Console.WriteLine($"{i} ");
+                }
+            }
+        }
+    }
+}
